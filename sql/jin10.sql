488bd16f1cb8c157a67757af8b879071bf807f9d
Yahua Yan <yanyahua@joinquant.com>
2016-12-24 11:07:49 +0800

    .

--- sql/jin10.sql
+++ sql/jin10.sql
@@ -4,6 +4,6 @@ create table if not exists `jin10_news` (
   /*`id` int(11) not null auto_increment,*/
   `id` bigint(25) not null,
   `html` text not null,
-  `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
   primary key (`id`)
 );
