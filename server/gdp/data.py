0c3e3f7902b767620cd24c9eb1a5c30531c9024d
yhyan <yanjiahua90@163.com>
2017-01-01 19:52:48 +0800

    .

--- server/gdp/data.py
+++ server/gdp/data.py
@@ -7,11 +7,13 @@ import os
 import math
 import datetime
 
-def get_data(N=1):
-    ff = os.listdir("data")
+CURDIR = os.path.abspath(os.path.dirname(__file__))
+DATADIR = os.path.join(CURDIR, "data")
+def get_data(N=10):
+    ff = os.listdir(DATADIR)
     data = {}
     for f in ff:
-        with open("data/%s" % f) as fp:
+        with open(os.path.join(DATADIR, f)) as fp:
             t = json.loads(fp.read())
             year = int(f.split(".")[0])
             data[year] = {}
@@ -19,20 +21,30 @@ def get_data(N=1):
                 cty = i['country']
                 gdp = i['gdp']
                 if gdp.endswith(u"万亿"):
-                    gdp = float(
-                        gdp.replace(u'万亿', '').encode("utf-8")) * 100000
+                    gdp = float(gdp.replace(u'万亿', '').encode("utf-8")) * 100000
                 elif gdp.endswith(u"亿"):
                     gdp = float(gdp.replace(u'亿', '').encode("utf-8")) * 10
                 elif gdp.endswith(u'千万'):
                     gdp = float(gdp.replace(u'千万', '').encode("utf-8"))
                 data[year][cty] = gdp
 
-    YEARS = sorted(data.keys())
-    topN = sorted(data[YEARS[-1]].items(),
-                  key=lambda i: i[1], reverse=True)[:N]
-    topN = [i[0] for i in topN]
-    print data
-    return topN
+    resp = {}
+    YEARS = sorted(data.keys())[20:]  # 去掉前面20年
+    topN = sorted(data[YEARS[-1]].items(), key=lambda i: i[1], reverse=True)[:N]
+    topN = [i[0] for i in topN]  # 名字
+    items = []
+    for name in topN:
+        item = {'name': name,
+                'type': 'line',
+               # 'stack': '总量',
+                'data': []}
+        for y in YEARS:
+            item['data'].append(data[y][name])
+        items.append(item)
+    resp['years'] = YEARS
+    resp['items'] = items
+    resp['names'] = topN
+    return resp
 
 if __name__ == "__main__":
     print get_data(5)
--- server/js/gdp.js
+++ server/js/gdp.js
@@ -1,13 +1,13 @@
 function get_option(opData){
     var option = {
         title: {
-            text: '各国gdp'
+            text: 'gdp(千万$)'
         },
         tooltip: {
             trigger: 'axis'
         },
         legend: {
-            data:['邮件营销','联盟广告','视频广告','直接访问','搜索引擎']
+            data:opData.names
         },
         grid: {
             left: '3%',
@@ -23,43 +23,12 @@ function get_option(opData){
         xAxis: {
             type: 'category',
             boundaryGap: false,
-            data: ['周一','周二','周三','周四','周五','周六','周日']
+            data: opData.years
         },
         yAxis: {
             type: 'value'
         },
-        series: [
-            {
-                name:'邮件营销',
-                type:'line',
-                stack: '总量',
-                data:[120, 132, 101, 134, 90, 230, 210]
-            },
-            {
-                name:'联盟广告',
-                type:'line',
-                stack: '总量',
-                data:[220, 182, 191, 234, 290, 330, 310]
-            },
-            {
-                name:'视频广告',
-                type:'line',
-                stack: '总量',
-                data:[150, 232, 201, 154, 190, 330, 410]
-            },
-            {
-                name:'直接访问',
-                type:'line',
-                stack: '总量',
-                data:[320, 332, 301, 334, 390, 330, 320]
-            },
-            {
-                name:'搜索引擎',
-                type:'line',
-                stack: '总量',
-                data:[820, 932, 901, 934, 1290, 1330, 1320]
-            }
-        ]
+        series: opData.items
     };
     return option;
 }
